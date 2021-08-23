de4ab385eb3791129c09c6bac21f9b142411b409
shahbazmughal <shahbaz.mughal@hostingshouse.com>
2021-08-23 17:28:13 +0500

    First Upload

---
+++ .cache/sync-requires.js
@@ -0,0 +1,25 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("D:\\wamp64\\www\\mumsgatsby\\theme\\site\\.cache\\dev-404-page.js")),
+  "component---packages-themes-gatsby-theme-flexiblocks-src-homepage-app-index-jsx": preferDefault(require("D:\\wamp64\\www\\mumsgatsby\\theme\\packages\\themes\\gatsby-theme-flexiblocks\\src\\homepage\\app\\index.jsx")),
+  "component---packages-themes-gatsby-theme-flexiblocks-src-homepage-consulting-index-jsx": preferDefault(require("D:\\wamp64\\www\\mumsgatsby\\theme\\packages\\themes\\gatsby-theme-flexiblocks\\src\\homepage\\consulting\\index.jsx")),
+  "component---packages-themes-gatsby-theme-flexiblocks-src-homepage-fintech-index-jsx": preferDefault(require("D:\\wamp64\\www\\mumsgatsby\\theme\\packages\\themes\\gatsby-theme-flexiblocks\\src\\homepage\\fintech\\index.jsx")),
+  "component---packages-themes-gatsby-theme-flexiblocks-src-homepage-marketing-index-jsx": preferDefault(require("D:\\wamp64\\www\\mumsgatsby\\theme\\packages\\themes\\gatsby-theme-flexiblocks\\src\\homepage\\marketing\\index.jsx")),
+  "component---packages-themes-gatsby-theme-flexiblocks-src-homepage-saas-index-jsx": preferDefault(require("D:\\wamp64\\www\\mumsgatsby\\theme\\packages\\themes\\gatsby-theme-flexiblocks\\src\\homepage\\saas\\index.jsx")),
+  "component---packages-themes-gatsby-theme-flexiblocks-src-homepage-saas-v-2-index-jsx": preferDefault(require("D:\\wamp64\\www\\mumsgatsby\\theme\\packages\\themes\\gatsby-theme-flexiblocks\\src\\homepage\\saas-v2\\index.jsx")),
+  "component---packages-themes-gatsby-theme-flexiblocks-src-innerpage-about-us-01-index-jsx": preferDefault(require("D:\\wamp64\\www\\mumsgatsby\\theme\\packages\\themes\\gatsby-theme-flexiblocks\\src\\innerpage\\about-us-01\\index.jsx")),
+  "component---packages-themes-gatsby-theme-flexiblocks-src-innerpage-about-us-02-index-jsx": preferDefault(require("D:\\wamp64\\www\\mumsgatsby\\theme\\packages\\themes\\gatsby-theme-flexiblocks\\src\\innerpage\\about-us-02\\index.jsx")),
+  "component---packages-themes-gatsby-theme-flexiblocks-src-innerpage-pricing-01-index-jsx": preferDefault(require("D:\\wamp64\\www\\mumsgatsby\\theme\\packages\\themes\\gatsby-theme-flexiblocks\\src\\innerpage\\pricing-01\\index.jsx")),
+  "component---packages-themes-gatsby-theme-flexiblocks-src-innerpage-pricing-02-index-jsx": preferDefault(require("D:\\wamp64\\www\\mumsgatsby\\theme\\packages\\themes\\gatsby-theme-flexiblocks\\src\\innerpage\\pricing-02\\index.jsx")),
+  "component---packages-themes-gatsby-theme-flexiblocks-src-innerpage-pricing-03-index-jsx": preferDefault(require("D:\\wamp64\\www\\mumsgatsby\\theme\\packages\\themes\\gatsby-theme-flexiblocks\\src\\innerpage\\pricing-03\\index.jsx")),
+  "component---packages-themes-gatsby-theme-flexiblocks-src-innerpage-services-01-index-jsx": preferDefault(require("D:\\wamp64\\www\\mumsgatsby\\theme\\packages\\themes\\gatsby-theme-flexiblocks\\src\\innerpage\\services-01\\index.jsx")),
+  "component---packages-themes-gatsby-theme-flexiblocks-src-innerpage-services-02-index-jsx": preferDefault(require("D:\\wamp64\\www\\mumsgatsby\\theme\\packages\\themes\\gatsby-theme-flexiblocks\\src\\innerpage\\services-02\\index.jsx")),
+  "component---packages-themes-gatsby-theme-flexiblocks-src-innerpage-services-03-index-jsx": preferDefault(require("D:\\wamp64\\www\\mumsgatsby\\theme\\packages\\themes\\gatsby-theme-flexiblocks\\src\\innerpage\\services-03\\index.jsx")),
+  "component---packages-themes-gatsby-theme-flexiblocks-src-pages-404-jsx": preferDefault(require("D:\\wamp64\\www\\mumsgatsby\\theme\\packages\\themes\\gatsby-theme-flexiblocks\\src\\pages\\404.jsx")),
+  "component---packages-themes-gatsby-theme-flexiblocks-src-pages-index-jsx": preferDefault(require("D:\\wamp64\\www\\mumsgatsby\\theme\\packages\\themes\\gatsby-theme-flexiblocks\\src\\pages\\index.jsx"))
+}
+
